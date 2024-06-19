3613d563825d793c4820a162a97d18cf56260eaf
João Vitor da Silva Correia <joaovitorsilvacorreia@gmail.com>
2024-06-18 23:36:08 -0300

    Add files via upload

---
+++ IFPR/LogarController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpr.control;
+
+import br.edu.ifpr.model.bean.Usuario;
+import br.edu.ifpr.model.dao.PessoaDAO;
+import br.edu.ifpr.model.util.PersistenceUtil;
+import br.edu.ifpr.view.LoginView;
+import br.edu.ifpr.view.MenuPrincipalView;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author sergio
+ */
+public class LogarController implements ActionListener {
+    private JTextField txtUsuario ; 
+    private JTextField txtSenha;
+    private LoginView loginView;
+    
+    private void limpaCampos(){
+        txtUsuario.setText("");
+        txtSenha.setText("");
+
+    }
+            
+
+    public LogarController(JTextField txtUsuario, JTextField txtSenha, LoginView loginView) {
+        this.txtUsuario = txtUsuario;
+        this.txtSenha = txtSenha;
+        this.loginView = loginView;
+                
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Usuario u = new Usuario();
+        PessoaDAO usuarioDAO = new PessoaDAO(PersistenceUtil.getEntityManager());
+        if (usuarioDAO.retrieveUsuario(txtSenha.getText(), txtUsuario.getText())) {
+            // Abrir a janela principal
+            loginView.setVisible(false);
+            MenuPrincipalView menuP = new MenuPrincipalView();
+            menuP.setVisible(true);
+            
+        } else {
+            // Mensagem de usuário inválido
+            JOptionPane.showMessageDialog(null,"Usuário ou Senha incorretos!");
+            limpaCampos();
+            
+        }
+                
+    }
+}
+            
---
+++ IFPR/PessoaDAO.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpr.model.dao;
+
+import br.edu.ifpr.model.bean.PessoaFisica;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+/**
+ *
+ * @author sergio
+ */
+public class PessoaDAO extends GenericDAO<Integer, PessoaDAO> {
+    
+    public PessoaDAO(EntityManager em) {
+        super(em);
+    }
+ 
+    public boolean retrieveUsuario(String senha, String userName){
+        TypedQuery<PessoaFisica> query = (TypedQuery<PessoaFisica>) em.createQuery(
+            "SELECT pf FROM PessoaFisica pf WHERE pf.senha = :senha AND pf.userName = :userName",PessoaFisica.class);
+        query.setParameter("userName", userName);
+        query.setParameter("senha", senha);
+        
+        List<PessoaFisica> pessoasFisica = query.getResultList();
+        if (pessoasFisica.size() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
