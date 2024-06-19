f3cb121bfb10f76a38e6b29ba6f9c5da1e7ef750
João Vitor da Silva Correia <joaovitorsilvacorreia@gmail.com>
2024-06-18 23:32:34 -0300

    AlunoMain.java

--- IFPR/AlunoMain.java
+++ IFPR/AlunoMain.java
@@ -6,7 +6,7 @@ public class principal {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Aluno aluno = new Aluno("Sergio", 1234, 200.0 );
+		Aluno aluno = new Aluno("João", 1234, 200.0 );
 		aluno.verificaAlunoPossuiBolsa();
 		aluno.alteraBolsa();
 		aluno.pagaMensalidade(400.0);
